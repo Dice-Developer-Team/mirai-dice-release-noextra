1a86644dd962e88824c0725a78def3479c9ae7bd
w4123 <1840686745@qq.com>
2021-04-07 21:16:59 +0800

    Updatte 20210407

--- LaunchMiraiTermux.sh
+++ LaunchMiraiTermux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 path=$(dirname "$0")
 cd "$path"
-LD_PRELOAD=./data/MiraiNative/CQP.dll java -Dmirai.slider.captcha.supported -cp "./libs/*" net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+java -Dmirai.slider.captcha.supported -cp "./libs/*" net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
