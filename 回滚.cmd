d62c83ae632f2bc4eb27e6fb2c974b1e49500dde
w4123 <1840686745@qq.com>
2021-09-24 19:14:27 +0800

    Try accept pwsh

--- 回滚.cmd
+++ 回滚.cmd
@@ -6,5 +6,19 @@ if not exist main.ps1 (
 	pause
 	goto :EOF
 )
-powershell -ExecutionPolicy Bypass .\main.ps1 -r
+set POWERSHELL_EXEC=powershell
+powershell -Command exit
+if errorlevel 1 (
+    echo 警告：找不到系统组件powershell，这可能代表系统已经损坏。正在使用pwsh
+    set POWERSHELL_EXEC=pwsh
+    pwsh -Command exit
+    if errorlevel 1 (
+	    echo 错误：找不到pwsh，请访问 https://aka.ms/powershell-release?tag=stable 手动安装PowerShell 7 后重试
+		echo 按任意键后将尝试打开下载页面（通常你需要下载并安装PowerShell-x.x.x-win-x64.msi）
+        pause
+		rundll32 url.dll,FileProtocolHandler https://aka.ms/powershell-release?tag=stable
+        exit
+    )
+)
+%POWERSHELL_EXEC% -ExecutionPolicy Bypass .\main.ps1 -r
 pause
